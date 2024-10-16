4511060cf34ea4eadbde85c0485d4152baf1c376
Mario Perhat <148371794+Mariocci@users.noreply.github.com>
2024-10-16 20:24:14 +0200

    Console app for listening

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var apiKey = "e724ae57-2b23-4115-a384-16b79a574072"; 
+
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5000/locationHub", options =>
+            {
+                options.Headers.Add("X-API-KEY", apiKey); 
+            })
+            .Build();
+
+        connection.On<string>("ReceiveLocationsUpdate", (location) =>
+        {
+            Console.WriteLine($"Locations update received: {location}");
+        });
+
+        await connection.StartAsync();
+        Console.WriteLine("Connected to SignalR hub.");
+
+        Console.ReadLine();
+    }
+}
